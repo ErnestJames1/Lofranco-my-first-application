ebc841a0eddf71f0e64e47d1c23a4a979d30d92e
Ernest James <ernestjames193@gmail.com>
2025-10-11 13:08:05 +0800

    Chapter 4: Implement Eloquent relationships

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,9 +6,22 @@
 <ul>
     @foreach ($jobs as $job)
         <li>
-            <a href="/job/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                {{ $job['title'] }} - ${{ number_format($job['salary']) }}
-            </a>
+            <a href="/job/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg hover:bg-gray-50">
+                    <div class="font-bold text-blue-500 text-sm">
+                        {{ $job->employer->name }}
+                    </div>
+                    <div>
+                        <strong class="text-laracasts">{{ $job['title'] }}</strong> — Pays {{ $job['salary'] }} per year.
+                    </div>
+
+                    <div class="mt-3">
+                        @foreach ($job->tags as $tag)
+                            <span class="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">
+                                {{ $tag->name }}
+                            </span>
+                        @endforeach
+                    </div>
+                </a>
         </li>
     @endforeach
 </ul>
